d7074dfa1855373497789da95deca65e993c5a60
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-07-29 19:24:35 +0530

    Time: 5 ms (72.29%), Space: 43.3 MB (95.49%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        if (word1 == null && word2 == null) return 0;
+        if (word1 == null || word2 == null) return word1 == null ? word2.length() : word1.length();
+        
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                    continue;
+                }
+                if (j == 0) {
+                    dp[i][j] = i;
+                    continue;
+                }
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else { // 3 ways to find replace, find min
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+
