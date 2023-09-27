21104fe1cc628d5da2e50432e713201e5904c570
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-09-27 09:07:45 +0530

    Time: 0 ms (100.00%), Space: 40 MB (96.41%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public String decodeAtIndex(String s, int k) {
+        long length = 0;
+        for (char c : s.toCharArray()) {
+            if (c >= 48 && c <= 57) {
+                length *= c - '0';
+            } else {
+                ++length;
+            }
+        }
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            k %= length;
+            if (c >= 48 && c <= 57) {
+                length /= c - '0';
+            } else if (k == 0) {
+                return String.valueOf(c);
+            } else {
+                --length;
+            }
+        }
+        return "";
+    }
+}
