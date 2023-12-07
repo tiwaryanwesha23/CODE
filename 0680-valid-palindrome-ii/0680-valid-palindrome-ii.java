90adef79d16bcee42bdac2370adf5f98c7cc05a1
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-12-07 20:19:01 +0530

    Time: 5 ms (85.35%), Space: 44.3 MB (64.07%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    private static final int UNUSED = -1;
+    private static final int USED = -2;
+    private static final int USING = -3;
+
+    public boolean validPalindrome(String s) {
+        char[] chs = s.toCharArray();
+        int len = chs.length;
+        int i = 0, j = len - 1;
+        int tmp = UNUSED;
+        int iTmp = -1, jTmp = -1;
+
+        while (i < j) {
+            if (chs[i] == chs[j]) {
+                i++;
+                j--;
+            } else if (tmp == UNUSED) {
+                tmp = USING;
+                iTmp = i + 1;
+                jTmp = j--;
+            } else if (tmp == USED) {
+                return false;
+            } else {
+                tmp = USED;
+                i = iTmp;
+                j = jTmp;
+            }
+        }
+
+        return true;
+    }
+
+}
