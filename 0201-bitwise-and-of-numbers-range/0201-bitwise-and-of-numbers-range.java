941a0a6955b588cc8d97d255bc2dee8ab12791e8
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2024-02-21 19:28:45 +0530

    Time: 3 ms (100.00%), Space: 43.9 MB (54.81%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        if(m == 0)  return 0;
+        int moveFactor = 0;
+        while(m != n) {
+            m >>= 1;
+            n >>= 1;
+            moveFactor ++;
+        }
+        return m << moveFactor;
+    }
+}
