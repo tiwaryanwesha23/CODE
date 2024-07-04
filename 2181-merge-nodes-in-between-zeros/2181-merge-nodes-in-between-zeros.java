30f6d807df2ab5cfcbfdb27cc89bbed2c60935b4
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2024-07-04 20:09:56 +0530

    Time: 6 ms (31.54%), Space: 82 MB (46.34%) - LeetHub

---
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode dummy = new ListNode();
+        int s = 0;
+        ListNode tail = dummy;
+        for (ListNode cur = head.next; cur != null; cur = cur.next) {
+            if (cur.val != 0) {
+                s += cur.val;
+            } else {
+                tail.next = new ListNode(s);
+                tail = tail.next;
+                s = 0;
+            }
+        }
+        return dummy.next;
+    }
+}
