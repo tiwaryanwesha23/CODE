76358cb5b6738e2010a2a93ba88de641a3c1c5cb
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2024-08-14 11:44:47 +0530

    Time: 367 ms (17.01%), Space: 45 MB (5.10%) - LeetHub

---
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        int len = nums.length;
+        int size = len * (len - 1) / 2;
+        int[] arr = new int[1000000];
+        for(int i = 0; i < len; i++){
+            for(int j = i + 1; j < len; j++){
+                arr[Math.abs(nums[i] - nums[j])]++;
+            }
+        }
+        int cur = 0;
+        while(k > 0){
+            k -= arr[cur++];
+        }
+        return cur - 1;
+    }
+}
