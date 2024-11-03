72e1baf3c71abe41d9f729dc0fbbfb323e7298f4
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2024-11-03 12:00:02 +0530

    Time: 1 ms (15.90%), Space: 41.7 MB (15.40%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean rotateString(String A, String B) {
+        // check edge condition
+        if (A == null || B == null || A.length() != B.length()) {
+            return false;
+        }
+        if (A.equals(B)) {
+            return true;
+        }
+        // move characters, one at a time, and compare
+        StringBuffer sb = new StringBuffer(A);
+        for (char c : A.toCharArray()) {
+            sb.deleteCharAt(0);
+            sb.append(c);
+            if (sb.toString().equals(B)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
